ebf82feae7df223e54b1cbd96905d20b0871be66
elifguney7 <elifguney13@gmail.com>
2024-05-15 00:49:45 +0300

    multi language

--- app.js
+++ app.js
@@ -1,12 +1,48 @@
 const express = require('express'); 
 
 const app = express(); 
-const PORT = 8080; 
+const PORT = 8080;
 
-app.get('/', (req, res)=>{ 
-	res.status(200); 
-	res.send("Welcome to root URL of Server"); 
-}); 
+const i18next = require('i18next');
+const FsBackend = require('i18next-fs-backend');
+const middleware = require('i18next-express-middleware');
+
+// Set EJS as the templating engine
+app.set('view engine', 'ejs');
+
+i18next
+  .use(FsBackend)
+  .init({
+    fallbackLng: 'en',
+    preload: ['en', 'tr'], // Preloaded languages
+    supportedLngs: ['en', 'tr'], // Languages you want to support
+    backend: {
+      loadPath: __dirname + '/locales/{{lng}}/{{ns}}.json'
+    }
+  });
+  console.log(__dirname); // Should output something like 'C:\Users\elifg\OneDrive\Masaüstü\web-final'
+
+// Use i18next middleware
+app.use(middleware.handle(i18next));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Define routes
+app.get('/', (req, res) => {
+    res.render('home', { i18n: res.locals });
+});
+
+// Change language route
+app.post('/change-lang', (req, res) => {
+    res.cookie('i18next', req.body.lang);
+    res.redirect('back');
+});
+
+// app.get('/', (req, res)=>{ 
+// 	res.status(200); 
+// 	res.send("Welcome to root URL of Server"); 
+// }); 
 
 app.listen(PORT, (error) =>{ 
 	if(!error) 
